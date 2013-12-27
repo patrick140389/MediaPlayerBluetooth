5c5cca207088ecd07d720e25c2ad98cfc41c8de7
Patrick Wessolowski <patrick.wessolowski@gmx.de>
2013-12-27 10:26:07 +0100

    Test from tablet

--- wessolowski-app-mediaplayerbluetooth/gen/com/wessolowski/app/util/R.java
+++ wessolowski-app-mediaplayerbluetooth/gen/com/wessolowski/app/util/R.java
@@ -4,17 +4,293 @@
  * aapt tool from the resource data it found.  It
  * should not be modified by hand.
  */
+
 package com.wessolowski.app.util;
 
 public final class R {
-	public static final class drawable {
-		public static final int ic_launcher = 0x7f020000;
-	}
-	public static final class string {
-		public static final int app_name = 0x7f040000;
-	}
-	public static final class style {
-		public static final int AppBaseTheme = 0x7f050000;
-		public static final int AppTheme = 0x7f050001;
-	}
+    public static final class attr {
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int imageHeight=0x7f010001;
+        /** <p>Must be a floating point value, such as "<code>1.2</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int imageReflectionRatio=0x7f010004;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int imageWidth=0x7f010000;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int reflectionGap=0x7f010003;
+        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int withReflection=0x7f010002;
+    }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
+    }
+    public static final class drawable {
+        public static final int ic_launcher=0x7f020000;
+        public static final int image0=0x7f020001;
+        public static final int image1=0x7f020002;
+        public static final int image2=0x7f020003;
+        public static final int image3=0x7f020004;
+        public static final int image4=0x7f020005;
+    }
+    public static final class id {
+        public static final int action_settings=0x7f08000b;
+        public static final int baselineDivider=0x7f080007;
+        public static final int baselineLayout=0x7f080006;
+        public static final int coverflow=0x7f080005;
+        public static final int headerLayout=0x7f080001;
+        public static final int headline=0x7f080002;
+        public static final int headlineDivider=0x7f080004;
+        public static final int mainLayout=0x7f080000;
+        public static final int playPause=0x7f080008;
+        public static final int test=0x7f080003;
+        public static final int titleBackward=0x7f080009;
+        public static final int titleForward=0x7f08000a;
+    }
+    public static final class layout {
+        public static final int activity_main=0x7f030000;
+    }
+    public static final class menu {
+        public static final int main=0x7f070000;
+    }
+    public static final class string {
+        public static final int action_settings=0x7f040001;
+        public static final int app_name=0x7f040000;
+        public static final int hello_world=0x7f040002;
+    }
+    public static final class style {
+        /** 
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
+         */
+        public static final int AppBaseTheme=0x7f050000;
+        /**  Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+         */
+        public static final int AppTheme=0x7f050001;
+    }
+    public static final class styleable {
+        /** Attributes that can be used with a CoverFlow.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #CoverFlow_imageHeight com.wessolowski.wessolowski.app.mediaplayerbluetooth:imageHeight}</code></td><td></td></tr>
+           <tr><td><code>{@link #CoverFlow_imageReflectionRatio com.wessolowski.wessolowski.app.mediaplayerbluetooth:imageReflectionRatio}</code></td><td></td></tr>
+           <tr><td><code>{@link #CoverFlow_imageWidth com.wessolowski.wessolowski.app.mediaplayerbluetooth:imageWidth}</code></td><td></td></tr>
+           <tr><td><code>{@link #CoverFlow_reflectionGap com.wessolowski.wessolowski.app.mediaplayerbluetooth:reflectionGap}</code></td><td></td></tr>
+           <tr><td><code>{@link #CoverFlow_withReflection com.wessolowski.wessolowski.app.mediaplayerbluetooth:withReflection}</code></td><td></td></tr>
+           </table>
+           @see #CoverFlow_imageHeight
+           @see #CoverFlow_imageReflectionRatio
+           @see #CoverFlow_imageWidth
+           @see #CoverFlow_reflectionGap
+           @see #CoverFlow_withReflection
+         */
+        public static final int[] CoverFlow = {
+            0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003,
+            0x7f010004
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.wessolowski.wessolowski.app.mediaplayerbluetooth.R.attr#imageHeight}
+          attribute's value can be found in the {@link #CoverFlow} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.wessolowski.wessolowski.app.mediaplayerbluetooth:imageHeight
+        */
+        public static final int CoverFlow_imageHeight = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.wessolowski.wessolowski.app.mediaplayerbluetooth.R.attr#imageReflectionRatio}
+          attribute's value can be found in the {@link #CoverFlow} array.
+
+
+          <p>Must be a floating point value, such as "<code>1.2</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.wessolowski.wessolowski.app.mediaplayerbluetooth:imageReflectionRatio
+        */
+        public static final int CoverFlow_imageReflectionRatio = 4;
+        /**
+          <p>This symbol is the offset where the {@link com.wessolowski.wessolowski.app.mediaplayerbluetooth.R.attr#imageWidth}
+          attribute's value can be found in the {@link #CoverFlow} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.wessolowski.wessolowski.app.mediaplayerbluetooth:imageWidth
+        */
+        public static final int CoverFlow_imageWidth = 0;
+        /**
+          <p>This symbol is the offset where the {@link com.wessolowski.wessolowski.app.mediaplayerbluetooth.R.attr#reflectionGap}
+          attribute's value can be found in the {@link #CoverFlow} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.wessolowski.wessolowski.app.mediaplayerbluetooth:reflectionGap
+        */
+        public static final int CoverFlow_reflectionGap = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.wessolowski.wessolowski.app.mediaplayerbluetooth.R.attr#withReflection}
+          attribute's value can be found in the {@link #CoverFlow} array.
+
+
+          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.wessolowski.wessolowski.app.mediaplayerbluetooth:withReflection
+        */
+        public static final int CoverFlow_withReflection = 2;
+    };
 }
